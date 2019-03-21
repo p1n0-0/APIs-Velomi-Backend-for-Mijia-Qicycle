d13f3fc58a69d877526de8be378a124cef158cef
Francisco Gómez Pino <fsgpino@gmail.com>
2019-03-21 18:24:39 +0100

    Up versión.

--- routes/bike.js
+++ routes/bike.js
@@ -4,11 +4,20 @@
 'use strict';
 
 // Dependencies
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
+const { getOTAs } = require('../lib/ota');
 
-// Obtain ota settings
-const { bms24, mc15, mc17, mc19 } = require('../settings').ota;
+/* GET List OTA files avaliable. */
+router.get('/ota.api', function(req, res, next) {
+
+  // Get params
+  const bmsSoftware = parseInt(req.body.bmsSoftware || req.param.bmsSoftware || req.query.bmsSoftware || 24);
+  const mcSoftware = parseInt(req.body.mcSoftware || req.param.mcSoftware || req.query.mcSoftware || 17);
+
+  // Render JSON
+  return res.json({ ota: getOTAs(bmsSoftware, mcSoftware) });
+
+});
 
 /* POST List OTA files avaliable. */
 router.post('/ota.api', function(req, res, next) {
@@ -17,32 +26,8 @@ router.post('/ota.api', function(req, res, next) {
   const bmsSoftware = parseInt(req.body.bmsSoftware || req.param.bmsSoftware || req.query.bmsSoftware || 24);
   const mcSoftware = parseInt(req.body.mcSoftware || req.param.mcSoftware || req.query.mcSoftware || 17);
 
-  let files = [];
-  let order = 1;
-
-  if (bmsSoftware && !isNaN(bmsSoftware) && bmsSoftware < 24) {
-    bms24.sort = order++; // Sort
-    files.push(bms24); // Update to bms24
-  }
-
-  if (mcSoftware === 15) {
-    mc17.sort = order++; // Sort
-    files.push(mc17); // Update to mc17 from mc15
-  }
-
-  if (mcSoftware === 17) {
-    mc19.sort = order++; // Sort
-    files.push(mc19); // Update to mc19 from mc17
-  }
-
-  if (mcSoftware === 19) {
-    mc17.software = 21; // Set fake software 21 to cheat app.
-    mc17.sort = order++; // Sort
-    files.push(mc17); // Downgrade to mc17 from mc19
-  }
-
   // Render JSON
-  return res.json({ ota: files });
+  return res.json({ ota: getOTAs(bmsSoftware, mcSoftware) });
 
 });
 
